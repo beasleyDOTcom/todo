e66f4364d5387b74ba5f889d34073d4691445569
Beasleydotcom <beasley.com@gmail.com>
2020-09-22 22:28:34 -0700

    finally got a working GET to fetch persistent data from API to render to the DOM

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -7,6 +7,7 @@ import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import axios from 'axios';
 
 import './todo.scss';
 
@@ -32,59 +33,66 @@ export default function TODO() {
     };
 
     useEffect(() => {
-        let list = [
-            { _id: 1, complete: false, text: 'Clean the Kitchen', difficulty: 3, assignee: 'Person A' },
-            { _id: 2, complete: false, text: 'Do the Laundry', difficulty: 2, assignee: 'Person A' },
-            { _id: 3, complete: false, text: 'Walk the Dog', difficulty: 4, assignee: 'Person C' },
-            { _id: 4, complete: true, text: 'Do Homework', difficulty: 3, assignee: 'Person C' },
-            { _id: 5, complete: false, text: 'Take a Nap', difficulty: 1, assignee: 'Person B' },
-        ];
-        setList(list)
+        // let list = [
+        //     { _id: 1, complete: false, text: 'Clean the Kitchen', difficulty: 3, assignee: 'Person A' },
+        //     { _id: 2, complete: false, text: 'Do the Laundry', difficulty: 2, assignee: 'Person A' },
+        //     { _id: 3, complete: false, text: 'Walk the Dog', difficulty: 4, assignee: 'Person C' },
+        //     { _id: 4, complete: true, text: 'Do Homework', difficulty: 3, assignee: 'Person C' },
+        //     { _id: 5, complete: false, text: 'Take a Nap', difficulty: 1, assignee: 'Person B' },
+        // ];
+        async function getSeedData() {
+            let response = {};
+            response = await axios.get('http://localhost:3005/api/v1/todos')
+            console.log('this is the response back from api', response)
+            setList(response.data.results)
+        }
+        getSeedData();
     }, []);
+    console.log('this is list', list)
     return (
         <>
             <header>
                 <Navbar bg="primary" variant="dark" className="mr-top-auto">
                     <Navbar.Brand href="#home">Home</Navbar.Brand>
- 
+
                 </Navbar>
                 {/* <h2>
                     There are {list.filter(item => !item.complete).length} Items To Complete
                 </h2> */}
             </header>
-        <Container>
+            <Container>
 
-            <Row>
-                <Navbar className="navy" bg="dark" variant="dark">
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                    <Nav className="mr-auto">
-                        {/* <Nav.Link href="#home">Home</Nav.Link> */}
-                        <h2>
-                            To Do List Manager({list.filter(item => !item.complete).length})
-                </h2>
-                    </Nav>
-                </Navbar>
-            </Row>
-            <Row>
-                <Col md={4}>
-                    <Card>
+                <Row>
+                    <Navbar className="navy" bg="dark" variant="dark">
+                        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                        <Nav className="mr-auto">
+                            {/* <Nav.Link href="#home">Home</Nav.Link> */}
+                            <h2>
+                                To Do List Manager({list.filter(item => !item.complete).length})
+                            </h2>
+                        </Nav>
+                    </Navbar>
+                </Row>
+                <Row>
+                    <Col md={4}>
+                        <Card>
 
+                            <div>
+                                <FormComponent handleSubmit={addItem} />
+                            </div>
+                        </Card>
+                    </Col>
+                    <Col md={8}>
                         <div>
-                            <FormComponent handleSubmit={addItem} />
+                            <TodoList
+                                list={list}
+                                handleComplete={toggleComplete}
+                            />
                         </div>
-                    </Card>
-                </Col>
-                <Col md={8}>
-                    <div>
-                        <TodoList
-                            list={list}
-                            handleComplete={toggleComplete}
-                        />
-                    </div>
-                </Col>
+                    </Col>
 
-            </Row>
-        </Container>
+                </Row>
+            </Container>
         </>
     )
 }
--- src/components/list.js
+++ src/components/list.js
@@ -1,13 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 export default function List(props) {
-    console.log(props.list, 'props dot list')
-    console.log(props, 'this is props')
     return (
         <ListGroup>
             {props.list.map((item) => (
-                console.log(item),
                 <ListGroup.Item action variant={item.complete ? "success": "danger"}
                     key={item._id}
                     onClick={() => props.handleComplete(item._id)}
